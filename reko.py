79dd8be06b7c1d849b992d5d818aaa6734bf605e
bdague324 <58032745+bdague324@users.noreply.github.com>
2020-01-30 15:09:21 +0100

    Create reko.py

---
+++ reko.py
@@ -0,0 +1,26 @@
+import boto3
+
+pic = 'Pic_name'
+
+client = boto3.client('rekognition',
+
+                      aws_access_key_id='your aws access key id',
+
+                      aws_secret_access_key='your aws secret access key',
+
+                      region_name='us-east-1')
+
+with open(pic, 'rb') as source_image:
+    source_bytes = source_image.read()
+
+response = client.detect_labels(Image={'Bytes': source_bytes}, MaxLabels=10, MinConfidence=90)
+
+
+def label():
+    for label in response['Labels']:
+        print("Label: " + label['Name'])
+
+        print("Confidence: " + str(label['Confidence']) + " %")
+
+
+label()
